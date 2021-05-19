bfb5d38b2495353729f5e5540040e6a996ea1d28
Serah wakaba <serahwakaba9@gmail.com>
2021-05-19 22:24:47 +0300

    passing parameters

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -5,6 +5,8 @@ import { AlertService } from '../alert-service/alert.service';
 import { Quote } from '../quote-class/quote';
 import { HttpClient } from '@angular/common/http';
 import { QuoteRequestService } from '../quote-http/quote-request.service';
+import { Router } from '@angular/router';
+
 
 
 
@@ -32,27 +34,33 @@ export class GoalComponent implements OnInit {
     goal.completeDate = new Date(goal.completeDate)
     this.goals.push(goal)
   }
-
-  toggleDetails(index){
-    this.goals[index].showDescription = !this.goals[index].showDescription;
-  }  completeGoal(isComplete, index){
-    if (isComplete) {
-      this.goals.splice(index,1);
-    }
+  goToUrl(id){
+    this.router.navigate(['/goals',id])
   }
-  deleteGoal(isComplete, index){
-    if (isComplete) {
-      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)     
-       if (toDelete){
-        this.goals.splice(index,1)
-        this.alertService.alertMe("The goal has been deleted")
 
-      }
+  deleteGoal(index){
+    let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}`)
+
+    if (toDelete){
+      this.goals.splice(index,1)
+      this.alertService.alertMe("Goal has been deleted")
     }
   }
 
-  constructor(goalService:GoalService, alertService:AlertService, private quoteService:QuoteRequestService) 
-  {
+  // 
+  
+  // deleteGoal(isComplete, index){
+  //   if (isComplete) {
+  //     let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)     
+  //      if (toDelete){
+  //       this.goals.splice(index,1)
+  //       this.alertService.alertMe("The goal has been deleted")
+
+  //     }
+  //   }
+  // }
+
+  constructor(goalService:GoalService, alertService:AlertService, private quoteService:QuoteRequestService, private router:Router) {
     this.goals = goalService.getGoals()
     this.alertService = alertService;
   }
