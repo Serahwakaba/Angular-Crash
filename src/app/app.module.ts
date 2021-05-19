af12b522f8546594a9721d27ebea09218c948566
Serah wakaba <serahwakaba9@gmail.com>
2021-05-19 20:55:52 +0300

    Not found commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { NgProgressModule } from '@ngx-progressbar/core';
 import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
 import { AboutComponent } from './about/about.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 
@@ -29,6 +30,7 @@ import { NavbarComponent } from './navbar/navbar.component';
     GoalFormComponent,
     AboutComponent,
     NavbarComponent,
+    NotFoundComponent,
 
     
   ],
